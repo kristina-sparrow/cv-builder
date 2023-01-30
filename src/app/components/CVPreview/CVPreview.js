5e74c817f712544951f254ddd64aa3c3c30aeb78
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-01-29 20:37:10 -0700

    Add education view component

--- src/app/components/CVPreview/CVPreview.js
+++ src/app/components/CVPreview/CVPreview.js
@@ -1,14 +1,14 @@
 import React from "react";
 import Personal from "./Personal";
-import Education from "./Education";
 import Experience from "./Experience";
+import Education from "./Education";
 
 export default function CVPreview() {
   return (
     <div>
       <Personal />
-      <Education />
       <Experience />
+      <Education />
     </div>
   );
 }
--- src/app/components/CVPreview/Education.js
+++ src/app/components/CVPreview/Education.js
@@ -1,5 +1,35 @@
 import React from "react";
 
-export default function Education() {
-  return <div></div>;
+export default function Education({ heading, educationList }) {
+  return (
+    <div className="education-list">
+      {heading ? <h3>{heading}</h3> : null}
+      {educationList.map((item, index) => {
+        const { program, school, completionDate, desc } = item;
+        return (
+          <EducationItem
+            key={index}
+            heading={program}
+            place={school}
+            completionDate={completionDate}
+            desc={desc}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function EducationItem({ heading, place, completionDate, desc }) {
+  return (
+    <div className="education">
+      <div className="education__heading">
+        <h4>{heading}</h4>
+        <div className="education__details">
+          {place} | <span className="bold">{completionDate}</span>
+        </div>
+      </div>
+      {desc ? desc : null}
+    </div>
+  );
 }
