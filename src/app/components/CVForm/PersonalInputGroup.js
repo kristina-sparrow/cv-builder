25bd5aeb8c34dbf26aa1cb62ecfdb6c44af5f3ce
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-01-30 13:45:59 -0700

    Match personal input group layout

--- src/app/components/CVForm/PersonalInputGroup.js
+++ src/app/components/CVForm/PersonalInputGroup.js
@@ -4,48 +4,51 @@ import TextAreaField from "./TextArea";
 
 export default function PersonalInputGroup({ personalInfo, onChange }) {
   return (
-    <div className="personal-group">
-      <InputField
-        type="text"
-        name="name"
-        placeholder="Full name"
-        value={personalInfo.name}
-        onChange={(e) => onChange(e)}
-      />
-      <InputField
-        type="text"
-        name="title"
-        placeholder="Title"
-        value={personalInfo.title}
-        onChange={(e) => onChange(e)}
-      />
-      <InputField
-        type="text"
-        name="phone"
-        placeholder="Phone number"
-        value={personalInfo.phone}
-        onChange={(e) => onChange(e)}
-      />
-      <InputField
-        type="text"
-        name="email"
-        placeholder="Email address"
-        value={personalInfo.email}
-        onChange={(e) => onChange(e)}
-      />
-      <InputField
-        type="text"
-        name="location"
-        placeholder="Location"
-        value={personalInfo.location}
-        onChange={(e) => onChange(e)}
-      />
-      <TextAreaField
-        name="description"
-        placeholder="Description"
-        value={personalInfo.description}
-        onChange={(e) => onChange(e)}
-      />
-    </div>
+    <section className="form-section">
+      <h2 className="section-title">Personal Details</h2>
+      <div className="personal-item">
+        <InputField
+          type="text"
+          name="name"
+          placeholder="Full name"
+          value={personalInfo.name}
+          onChange={(e) => onChange(e)}
+        />
+        <InputField
+          type="text"
+          name="title"
+          placeholder="Title"
+          value={personalInfo.title}
+          onChange={(e) => onChange(e)}
+        />
+        <InputField
+          type="text"
+          name="phone"
+          placeholder="Phone number"
+          value={personalInfo.phone}
+          onChange={(e) => onChange(e)}
+        />
+        <InputField
+          type="text"
+          name="email"
+          placeholder="Email address"
+          value={personalInfo.email}
+          onChange={(e) => onChange(e)}
+        />
+        <InputField
+          type="text"
+          name="location"
+          placeholder="Location"
+          value={personalInfo.location}
+          onChange={(e) => onChange(e)}
+        />
+        <TextAreaField
+          name="description"
+          placeholder="Description"
+          value={personalInfo.description}
+          onChange={(e) => onChange(e)}
+        />
+      </div>
+    </section>
   );
 }
