833601c37f72e6fef62f9c3ad7c69e6cc1fd6fdd
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-01-30 16:59:05 -0700

    Install babel and fix webpack errors

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app/App.js";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
